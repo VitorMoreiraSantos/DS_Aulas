091d05e230d0ac45e600c51b4646292fb614c8ca
Vitor <vittor.moreira98@gmail.com>
2024-02-09 20:32:21 -0300

    Adicionando arquivos

---
+++ Aula02/Aula02/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Aula02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonFechar = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menu1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subMenu1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subMenu2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subMenu21ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subMenu22ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subMenu23ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.labelNascimento = new System.Windows.Forms.Label();
+            this.dateTimePickerNascimento = new System.Windows.Forms.DateTimePicker();
+            this.buttonExibir = new System.Windows.Forms.Button();
+            this.labelSexo = new System.Windows.Forms.Label();
+            this.comboBoxSexo = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonFechar
+            // 
+            this.buttonFechar.BackColor = System.Drawing.Color.Red;
+            this.buttonFechar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonFechar.Location = new System.Drawing.Point(702, 36);
+            this.buttonFechar.Name = "buttonFechar";
+            this.buttonFechar.Size = new System.Drawing.Size(86, 34);
+            this.buttonFechar.TabIndex = 0;
+            this.buttonFechar.Text = "Fechar";
+            this.buttonFechar.UseVisualStyleBackColor = false;
+            this.buttonFechar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu1ToolStripMenuItem,
+            this.menu2ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menu1ToolStripMenuItem
+            // 
+            this.menu1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.subMenu1ToolStripMenuItem,
+            this.subMenu2ToolStripMenuItem});
+            this.menu1ToolStripMenuItem.Name = "menu1ToolStripMenuItem";
+            this.menu1ToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.menu1ToolStripMenuItem.Text = "Menu&1";
+            // 
+            // subMenu1ToolStripMenuItem
+            // 
+            this.subMenu1ToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("subMenu1ToolStripMenuItem.Image")));
+            this.subMenu1ToolStripMenuItem.Name = "subMenu1ToolStripMenuItem";
+            this.subMenu1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.subMenu1ToolStripMenuItem.Text = "SubMenu&11";
+            // 
+            // subMenu2ToolStripMenuItem
+            // 
+            this.subMenu2ToolStripMenuItem.Name = "subMenu2ToolStripMenuItem";
+            this.subMenu2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.subMenu2ToolStripMenuItem.Text = "SubMenu&12";
+            // 
+            // menu2ToolStripMenuItem
+            // 
+            this.menu2ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.subMenu21ToolStripMenuItem,
+            this.subMenu22ToolStripMenuItem,
+            this.subMenu23ToolStripMenuItem});
+            this.menu2ToolStripMenuItem.Name = "menu2ToolStripMenuItem";
+            this.menu2ToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.menu2ToolStripMenuItem.Text = "Menu&2";
+            // 
+            // subMenu21ToolStripMenuItem
+            // 
+            this.subMenu21ToolStripMenuItem.Name = "subMenu21ToolStripMenuItem";
+            this.subMenu21ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.subMenu21ToolStripMenuItem.Text = "SubMenu&21";
+            // 
+            // subMenu22ToolStripMenuItem
+            // 
+            this.subMenu22ToolStripMenuItem.Name = "subMenu22ToolStripMenuItem";
+            this.subMenu22ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.subMenu22ToolStripMenuItem.Text = "SubMenu&22";
+            // 
+            // subMenu23ToolStripMenuItem
+            // 
+            this.subMenu23ToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("subMenu23ToolStripMenuItem.Image")));
+            this.subMenu23ToolStripMenuItem.Name = "subMenu23ToolStripMenuItem";
+            this.subMenu23ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.subMenu23ToolStripMenuItem.Text = "SubMenu23";
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNome.Location = new System.Drawing.Point(26, 46);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(62, 24);
+            this.labelNome.TabIndex = 2;
+            this.labelNome.Text = "Nome";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNome.Location = new System.Drawing.Point(30, 74);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(404, 26);
+            this.textBoxNome.TabIndex = 3;
+            // 
+            // labelNascimento
+            // 
+            this.labelNascimento.AutoSize = true;
+            this.labelNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNascimento.Location = new System.Drawing.Point(26, 122);
+            this.labelNascimento.Name = "labelNascimento";
+            this.labelNascimento.Size = new System.Drawing.Size(179, 24);
+            this.labelNascimento.TabIndex = 4;
+            this.labelNascimento.Text = "Data de Nascimento";
+            // 
+            // dateTimePickerNascimento
+            // 
+            this.dateTimePickerNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerNascimento.Location = new System.Drawing.Point(30, 149);
+            this.dateTimePickerNascimento.Name = "dateTimePickerNascimento";
+            this.dateTimePickerNascimento.Size = new System.Drawing.Size(138, 26);
+            this.dateTimePickerNascimento.TabIndex = 5;
+            // 
+            // buttonExibir
+            // 
+            this.buttonExibir.BackColor = System.Drawing.Color.Blue;
+            this.buttonExibir.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonExibir.Location = new System.Drawing.Point(702, 223);
+            this.buttonExibir.Name = "buttonExibir";
+            this.buttonExibir.Size = new System.Drawing.Size(86, 34);
+            this.buttonExibir.TabIndex = 6;
+            this.buttonExibir.Text = "Exibir";
+            this.buttonExibir.UseVisualStyleBackColor = false;
+            this.buttonExibir.Click += new System.EventHandler(this.buttonExibir_Click);
+            // 
+            // labelSexo
+            // 
+            this.labelSexo.AutoSize = true;
+            this.labelSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSexo.Location = new System.Drawing.Point(255, 122);
+            this.labelSexo.Name = "labelSexo";
+            this.labelSexo.Size = new System.Drawing.Size(54, 24);
+            this.labelSexo.TabIndex = 7;
+            this.labelSexo.Text = "Sexo";
+            // 
+            // comboBoxSexo
+            // 
+            this.comboBoxSexo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSexo.Items.AddRange(new object[] {
+            "Feminino",
+            "Masculino"});
+            this.comboBoxSexo.Location = new System.Drawing.Point(259, 153);
+            this.comboBoxSexo.Name = "comboBoxSexo";
+            this.comboBoxSexo.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSexo.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.comboBoxSexo);
+            this.Controls.Add(this.labelSexo);
+            this.Controls.Add(this.buttonExibir);
+            this.Controls.Add(this.dateTimePickerNascimento);
+            this.Controls.Add(this.labelNascimento);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.buttonFechar);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFechar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menu1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subMenu1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subMenu2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menu2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subMenu21ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subMenu22ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subMenu23ToolStripMenuItem;
+        private System.Windows.Forms.Label labelNome;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.Label labelNascimento;
+        private System.Windows.Forms.DateTimePicker dateTimePickerNascimento;
+        private System.Windows.Forms.Button buttonExibir;
+        private System.Windows.Forms.Label labelSexo;
+        private System.Windows.Forms.ComboBox comboBoxSexo;
+    }
+}
+
---
+++ Aula02/Aula02/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Você deseja fechar a janela?", "Fechar Janela", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void buttonExibir_Click(object sender, EventArgs e)
+        {
+            
+            string nome = textBoxNome.Text;
+
+            string dataNascimento = dateTimePickerNascimento.Value.ToString("dd/MM/yyyy");
+
+            string sexo = comboBoxSexo.SelectedItem.ToString();
+
+            string mensagem = $"Nome: {nome}\nData de Nascimento: {dataNascimento}\nSexo: {sexo}";
+
+            MessageBox.Show(mensagem, "Informações Fornecidas");
+        }
+    }
+}
