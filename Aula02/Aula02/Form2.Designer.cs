26a63aab4a00662a9e403fe506b6d7b3a0c4d13c
Vitor <vittor.moreira98@gmail.com>
2024-02-09 22:19:18 -0300

    Complemento da Aula

--- Aula02/Aula02/Form2.Designer.cs
+++ Aula02/Aula02/Form2.Designer.cs
@@ -1,16 +1,16 @@
 ﻿namespace Aula02
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
-        /// Variável de designer necessária.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Limpar os recursos que estão sendo usados.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,53 +20,31 @@
             base.Dispose(disposing);
         }
 
-        #region Código gerado pelo Windows Form Designer
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Método necessário para suporte ao Designer - não modifique 
-        /// o conteúdo deste método com o editor de código.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.buttonFechar = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menu1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.subMenu1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.subMenu2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menu2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.subMenu21ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.labelNome = new System.Windows.Forms.Label();
-            this.textBoxNome = new System.Windows.Forms.TextBox();
-            this.labelNascimento = new System.Windows.Forms.Label();
-            this.dateTimePickerNascimento = new System.Windows.Forms.DateTimePicker();
             this.buttonExibir = new System.Windows.Forms.Button();
-            this.labelSexo = new System.Windows.Forms.Label();
-            this.comboBoxSexo = new System.Windows.Forms.ComboBox();
+            this.textBoxProduto = new System.Windows.Forms.TextBox();
+            this.labelProduto = new System.Windows.Forms.Label();
+            this.buttonFechar = new System.Windows.Forms.Button();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.tssMensagem = new System.Windows.Forms.ToolStripStatusLabel();
-            this.tssData = new System.Windows.Forms.ToolStripStatusLabel();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.menuStrip1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
-            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // buttonFechar
-            // 
-            this.buttonFechar.BackColor = System.Drawing.Color.Red;
-            this.buttonFechar.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.buttonFechar.Location = new System.Drawing.Point(702, 52);
-            this.buttonFechar.Name = "buttonFechar";
-            this.buttonFechar.Size = new System.Drawing.Size(86, 34);
-            this.buttonFechar.TabIndex = 0;
-            this.buttonFechar.Text = "Fechar";
-            this.buttonFechar.UseVisualStyleBackColor = false;
-            this.buttonFechar.Click += new System.EventHandler(this.button1_Click);
             // 
             // menuStrip1
             // 
@@ -77,7 +55,7 @@
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // menu1ToolStripMenuItem
@@ -116,48 +94,9 @@
             // subMenu21ToolStripMenuItem
             // 
             this.subMenu21ToolStripMenuItem.Name = "subMenu21ToolStripMenuItem";
-            this.subMenu21ToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.subMenu21ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.subMenu21ToolStripMenuItem.Text = "Sobre";
             this.subMenu21ToolStripMenuItem.Click += new System.EventHandler(this.subMenu21ToolStripMenuItem_Click);
-            // 
-            // labelNome
-            // 
-            this.labelNome.AutoSize = true;
-            this.labelNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelNome.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.labelNome.Location = new System.Drawing.Point(17, 79);
-            this.labelNome.Name = "labelNome";
-            this.labelNome.Size = new System.Drawing.Size(158, 24);
-            this.labelNome.TabIndex = 2;
-            this.labelNome.Text = "Nome do Usuário";
-            // 
-            // textBoxNome
-            // 
-            this.textBoxNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxNome.Location = new System.Drawing.Point(21, 107);
-            this.textBoxNome.Name = "textBoxNome";
-            this.textBoxNome.Size = new System.Drawing.Size(404, 26);
-            this.textBoxNome.TabIndex = 3;
-            // 
-            // labelNascimento
-            // 
-            this.labelNascimento.AutoSize = true;
-            this.labelNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelNascimento.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.labelNascimento.Location = new System.Drawing.Point(17, 155);
-            this.labelNascimento.Name = "labelNascimento";
-            this.labelNascimento.Size = new System.Drawing.Size(179, 24);
-            this.labelNascimento.TabIndex = 4;
-            this.labelNascimento.Text = "Data de Nascimento";
-            // 
-            // dateTimePickerNascimento
-            // 
-            this.dateTimePickerNascimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePickerNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePickerNascimento.Location = new System.Drawing.Point(21, 182);
-            this.dateTimePickerNascimento.Name = "dateTimePickerNascimento";
-            this.dateTimePickerNascimento.Size = new System.Drawing.Size(138, 26);
-            this.dateTimePickerNascimento.TabIndex = 5;
             // 
             // buttonExibir
             // 
@@ -166,33 +105,41 @@
             this.buttonExibir.Location = new System.Drawing.Point(702, 223);
             this.buttonExibir.Name = "buttonExibir";
             this.buttonExibir.Size = new System.Drawing.Size(86, 34);
-            this.buttonExibir.TabIndex = 6;
+            this.buttonExibir.TabIndex = 14;
             this.buttonExibir.Text = "Exibir";
             this.buttonExibir.UseVisualStyleBackColor = false;
             this.buttonExibir.Click += new System.EventHandler(this.buttonExibir_Click);
             // 
-            // labelSexo
+            // textBoxProduto
             // 
-            this.labelSexo.AutoSize = true;
-            this.labelSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSexo.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.labelSexo.Location = new System.Drawing.Point(300, 153);
-            this.labelSexo.Name = "labelSexo";
-            this.labelSexo.Size = new System.Drawing.Size(54, 24);
-            this.labelSexo.TabIndex = 7;
-            this.labelSexo.Text = "Sexo";
+            this.textBoxProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxProduto.Location = new System.Drawing.Point(21, 107);
+            this.textBoxProduto.Name = "textBoxProduto";
+            this.textBoxProduto.Size = new System.Drawing.Size(404, 26);
+            this.textBoxProduto.TabIndex = 11;
             // 
-            // comboBoxSexo
+            // labelProduto
             // 
-            this.comboBoxSexo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBoxSexo.Items.AddRange(new object[] {
-            "Feminino",
-            "Masculino"});
-            this.comboBoxSexo.Location = new System.Drawing.Point(304, 180);
-            this.comboBoxSexo.Name = "comboBoxSexo";
-            this.comboBoxSexo.Size = new System.Drawing.Size(121, 28);
-            this.comboBoxSexo.TabIndex = 8;
+            this.labelProduto.AutoSize = true;
+            this.labelProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProduto.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.labelProduto.Location = new System.Drawing.Point(17, 79);
+            this.labelProduto.Name = "labelProduto";
+            this.labelProduto.Size = new System.Drawing.Size(160, 24);
+            this.labelProduto.TabIndex = 10;
+            this.labelProduto.Text = "Nome do Produto";
+            // 
+            // buttonFechar
+            // 
+            this.buttonFechar.BackColor = System.Drawing.Color.Red;
+            this.buttonFechar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonFechar.Location = new System.Drawing.Point(702, 52);
+            this.buttonFechar.Name = "buttonFechar";
+            this.buttonFechar.Size = new System.Drawing.Size(86, 34);
+            this.buttonFechar.TabIndex = 9;
+            this.buttonFechar.Text = "Fechar";
+            this.buttonFechar.UseVisualStyleBackColor = false;
+            this.buttonFechar.Click += new System.EventHandler(this.buttonFechar_Click);
             // 
             // toolStrip1
             // 
@@ -203,7 +150,7 @@
             this.toolStrip1.Location = new System.Drawing.Point(356, 24);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(58, 25);
-            this.toolStrip1.TabIndex = 9;
+            this.toolStrip1.TabIndex = 15;
             this.toolStrip1.Text = "CadProd";
             // 
             // toolStripButton1
@@ -226,92 +173,46 @@
             this.toolStripButton2.Text = "toolStripButtonCadUser";
             this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
             // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tssMensagem,
-            this.tssData});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
-            this.statusStrip1.TabIndex = 10;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // tssMensagem
-            // 
-            this.tssMensagem.Name = "tssMensagem";
-            this.tssMensagem.Size = new System.Drawing.Size(66, 17);
-            this.tssMensagem.Text = "Mensagem";
-            // 
-            // tssData
-            // 
-            this.tssData.Name = "tssData";
-            this.tssData.Size = new System.Drawing.Size(31, 17);
-            this.tssData.Text = "Data";
-            // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // Form1
+            // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
-            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.comboBoxSexo);
-            this.Controls.Add(this.labelSexo);
             this.Controls.Add(this.buttonExibir);
-            this.Controls.Add(this.dateTimePickerNascimento);
-            this.Controls.Add(this.labelNascimento);
-            this.Controls.Add(this.textBoxNome);
-            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.textBoxProduto);
+            this.Controls.Add(this.labelProduto);
             this.Controls.Add(this.buttonFechar);
             this.Controls.Add(this.menuStrip1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "Form1";
+            this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Form2";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button buttonFechar;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem menu1ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem subMenu1ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem subMenu2ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem menu2ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem subMenu21ToolStripMenuItem;
-        private System.Windows.Forms.Label labelNome;
-        private System.Windows.Forms.TextBox textBoxNome;
-        private System.Windows.Forms.Label labelNascimento;
-        private System.Windows.Forms.DateTimePicker dateTimePickerNascimento;
         private System.Windows.Forms.Button buttonExibir;
-        private System.Windows.Forms.Label labelSexo;
-        private System.Windows.Forms.ComboBox comboBoxSexo;
+        private System.Windows.Forms.TextBox textBoxProduto;
+        private System.Windows.Forms.Label labelProduto;
+        private System.Windows.Forms.Button buttonFechar;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.ToolStripButton toolStripButton2;
-        private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ToolStripStatusLabel tssMensagem;
-        private System.Windows.Forms.ToolStripStatusLabel tssData;
-        private System.Windows.Forms.Timer timer1;
     }
 }
-
--- Aula02/Aula02/Form2.cs
+++ Aula02/Aula02/Form2.cs
@@ -10,15 +10,19 @@ using System.Windows.Forms;
 
 namespace Aula02
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-            timer1.Start();
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonFechar_Click(object sender, EventArgs e)
         {
             DialogResult result = MessageBox.Show("Você deseja fechar a janela?", "Fechar Janela", MessageBoxButtons.YesNo);
 
@@ -30,40 +34,12 @@ namespace Aula02
 
         private void buttonExibir_Click(object sender, EventArgs e)
         {
-            
-            string nome = textBoxNome.Text;
 
-            string dataNascimento = dateTimePickerNascimento.Value.ToString("dd/MM/yyyy");
+            string nome = textBoxProduto.Text;
 
-            string sexo = comboBoxSexo.SelectedItem.ToString();
-
-            string mensagem = $"Nome: {nome}\nData de Nascimento: {dataNascimento}\nSexo: {sexo}";
+            string mensagem = $"Nome do Produto: {nome}";
 
             MessageBox.Show(mensagem, "Informações Fornecidas");
-        }
-
-        private void subMenu21ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            AboutBox1 sobre = new AboutBox1();
-            sobre.ShowDialog();
-        }
-
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            tssData.Text = "Data/Hora: " + DateTime.Now.ToString();
-        }
-
-        private void subMenu1ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Form2 produtotela = new Form2();
-            produtotela.ShowDialog();
-        }
-
-        private void subMenu2ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Form1 usuariotela = new Form1();
-            usuariotela.ShowDialog();
         }
 
         private void toolStripButton1_Click(object sender, EventArgs e)
@@ -73,6 +49,24 @@ namespace Aula02
         }
 
         private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            Form1 usuariotela = new Form1();
+            usuariotela.ShowDialog();
+        }
+
+        private void subMenu21ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 sobre = new AboutBox1();
+            sobre.ShowDialog();
+        }
+
+        private void subMenu1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 produtotela = new Form2();
+            produtotela.ShowDialog();
+        }
+
+        private void subMenu2ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Form1 usuariotela = new Form1();
             usuariotela.ShowDialog();
