39c0646c211213d041bc7b51116268a149b3f87b
Vitor <vittor.moreira98@gmail.com>
2024-03-07 21:02:54 -0300

    Aula de C# 07/03/2024

---
+++ DSI/Aula03/aula20240307/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace aula20240307
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 80;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 120;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 60;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::aula20240307.Properties.Resources.botao;
+            this.button1.Location = new System.Drawing.Point(636, 332);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::aula20240307.Properties.Resources.limao;
+            this.pictureBox4.Location = new System.Drawing.Point(419, 152);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::aula20240307.Properties.Resources.cereja;
+            this.pictureBox2.Location = new System.Drawing.Point(246, 152);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::aula20240307.Properties.Resources.sete;
+            this.pictureBox3.Location = new System.Drawing.Point(74, 152);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::aula20240307.Properties.Resources.fundo;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(882, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(761, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 36);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Parar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 449);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DSI/Aula03/aula20240307/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula20240307
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random gera = new Random();
+
+            int x = gera.Next(0, 6);
+
+            if (x >= 6)
+            {
+                pictureBox4.Image = Properties.Resources.limao;
+            }else if(x == 5)
+            {
+                pictureBox4.Image = Properties.Resources.melancia;
+            }
+            else if (x == 4)
+            {
+                pictureBox4.Image = Properties.Resources.cereja;
+            }
+            else if (x == 3)
+            {
+                pictureBox4.Image = Properties.Resources.cereja_azul;
+            }
+            else if (x <= 2)
+            {
+                pictureBox4.Image = Properties.Resources.sete;
+            }
+
+        }
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random gera = new Random();
+
+            int x = gera.Next(0, 6);
+
+            if (x >= 6)
+            {
+                pictureBox2.Image = Properties.Resources.limao;
+            }
+            else if (x == 5)
+            {
+                pictureBox2.Image = Properties.Resources.melancia;
+            }
+            else if (x == 4)
+            {
+                pictureBox2.Image = Properties.Resources.cereja;
+            }
+            else if (x == 3)
+            {
+                pictureBox2.Image = Properties.Resources.cereja_azul;
+            }
+            else if (x <= 2)
+            {
+                pictureBox2.Image = Properties.Resources.sete;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            Random gera = new Random();
+
+            int x = gera.Next(0, 6);
+
+            if (x >= 6)
+            {
+                pictureBox3.Image = Properties.Resources.limao;
+            }
+            else if (x == 5)
+            {
+                pictureBox3.Image = Properties.Resources.melancia;
+            }
+            else if (x == 4)
+            {
+                pictureBox3.Image = Properties.Resources.cereja;
+            }
+            else if (x == 3)
+            {
+                pictureBox3.Image = Properties.Resources.cereja_azul;
+            }
+            else if (x <= 2)
+            {
+                pictureBox3.Image = Properties.Resources.sete;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+            //timer1.Tick += new EventHandler(timer1_disable);
+            //timer2.Tick += new EventHandler(timer2_disable);
+            //timer3.Tick += new EventHandler(timer3_disable);
+        }
+
+        private void timer1_disable(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+        private void timer2_disable(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+        }
+        private void timer3_disable(object sender, EventArgs e)
+        {
+            timer3.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+        }
+    }
+}
