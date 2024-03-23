d825666e5520e5459aa607567180de26a74ec384
Vitor <vittor.moreira98@gmail.com>
2024-03-22 21:47:35 -0300

    Aula 05 DSI

---
+++ DSI/Aula05/ValidadorCPF/ValidadorCPF/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidadorCPF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cpf = maskedTextBox1.Text;
+
+            bool verifica = true;
+
+            verifica = Validacoes.CPFValido(cpf);
+            if (verifica == true)
+            {
+                MessageBox.Show("CPF válido!");
+            }
+            else
+            {
+                MessageBox.Show("CPF inválido!");
+            }
+
+        }
+    }
+}
